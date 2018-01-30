d9b6fedb9fa5d84c5d1de731108341d03cf9848e
Larry Neal <nealla@lwneal.com>
2018-01-29 17:48:07 -0800

    Add counterfactual generation code

---
+++ src/counterfactual.py
@@ -0,0 +1,141 @@
+import os
+import random
+import time
+import torch
+import numpy as np
+from torch import autograd
+from torch.autograd import Variable
+from torch.nn.functional import softmax, sigmoid, log_softmax
+from torch.nn.functional import nll_loss, cross_entropy
+from vector import gen_noise, clamp_to_unit_sphere
+import imutil
+from imutil import VideoMaker
+
+
+def to_torch(z, requires_grad=False):
+    return Variable(torch.FloatTensor(z), requires_grad=requires_grad).cuda()
+
+
+def to_np(z):
+    return z.data.cpu().numpy()
+
+
+# Just picks ground-truth examples with the right labels
+def rejection_sample(networks, dataloader, **options):
+    result_dir = options['result_dir']
+
+    # Generate a K by K square grid of examples, one column per class
+    K = dataloader.num_classes
+    images = [[] for _ in range(N)]
+    for img_batch, label_batch, _ in dataloader:
+        for img, label in zip(img_batch, label_batch):
+            if label < N and len(images[label]) < N:
+                images[label].append(img.cpu().numpy())
+        if all(len(images[i]) == N for i in range(N)):
+            break
+
+    flat = []
+    for i in range(N):
+        for j in range(N):
+            flat.append(images[j][i])
+    images = flat
+
+    images = np.array(images).transpose((0,2,3,1))
+    start_class = 0
+    video_filename = make_video_filename(result_dir, dataloader, start_class, start_class, label_type='grid')
+    # Save the images in npy format to re-load as training data
+    trajectory_filename = video_filename.replace('.mjpeg', '.npy')
+    np.save(trajectory_filename, images)
+    # Save the images in jpg format to display to the user
+    imutil.show(images, filename=video_filename.replace('.mjpeg', '.jpg'))
+    return images
+
+
+# Generates 'counterfactual' images for each class, by gradient descent of the class
+def generate_counterfactual(networks, dataloader, **options):
+    # ISSUE: Unexpected BatchNorm behavior causes bad output if .eval() is set
+    """
+    for net in networks:
+        networks[net].eval()
+    """
+    result_dir = options['result_dir']
+
+    K = dataloader.num_classes
+
+    batches = []
+    for class_idx in range(K):
+        img_batch = generate_images_for_class(networks, dataloader, class_idx, **options)
+        batches.append(img_batch)
+
+    images = []
+    for i in range(K):
+        for batch in batches:
+            images.append(batch[i])
+
+    images = np.array(images).transpose((0,2,3,1))
+    dummy_class = 0
+    video_filename = make_video_filename(result_dir, dataloader, dummy_class, dummy_class, label_type='grid')
+
+    # Save the images in npy format to re-load as training data
+    trajectory_filename = video_filename.replace('.mjpeg', '.npy')
+    np.save(trajectory_filename, images)
+
+    # Save the images in jpg format to display to the user
+    imutil.show(images, filename=video_filename.replace('.mjpeg', '.jpg'))
+    return images
+
+
+def generate_images_for_class(networks, dataloader, class_idx, **options):
+    netG = networks['generator']
+    netD = networks['discriminator']
+    result_dir = options['result_dir']
+    latent_size = options['latent_size']
+    speed = options['cf_speed']
+    max_iters = options['cf_max_iters']
+
+    # Start with K random points
+    K = dataloader.num_classes
+    z = gen_noise(K, latent_size)
+    z = Variable(z, requires_grad=True).cuda()
+
+    # Move them so their labels match target_label
+    target_label = torch.LongTensor(K)
+    target_label[:] = class_idx
+    target_label = Variable(target_label).cuda()
+
+    for i in range(max_iters):
+        images = netG(z)
+        net_y = netD(images)
+        preds = softmax(net_y, dim=1)
+
+        pred_classes = to_np(preds.max(1)[1])
+        predicted_class = pred_classes[0]
+        pred_confidences = to_np(preds.max(1)[0])
+        pred_confidence = pred_confidences[0]
+        predicted_class_name = dataloader.lab_conv.labels[predicted_class]
+        print("Class: {} ({:.3f} confidence). Target class {}".format(
+            predicted_class_name, pred_confidence, class_idx))
+
+        cf_loss = nll_loss(log_softmax(net_y, dim=1), target_label)
+
+        dc_dz = autograd.grad(cf_loss, z, cf_loss, retain_graph=True)[0]
+        z -= dc_dz * speed
+        z = clamp_to_unit_sphere(z)
+        if all(pred_classes == class_idx) and all(pred_confidences > 0.75):
+            break
+    return images.data.cpu().numpy()
+
+
+# Trajectories are written to result_dir/trajectories/
+def make_video_filename(result_dir, dataloader, start_class, target_class, label_type='active'):
+    trajectory_id = '{}_{}'.format(dataloader.dsf.name, int(time.time() * 1000))
+    start_class_name = dataloader.lab_conv.labels[start_class]
+    target_class_name = dataloader.lab_conv.labels[target_class]
+    video_filename = '{}-{}-{}-{}.mjpeg'.format(label_type, trajectory_id, start_class_name, target_class_name)
+    video_filename = os.path.join('trajectories', video_filename)
+    video_filename = os.path.join(result_dir, video_filename)
+    path = os.path.join(result_dir, 'trajectories')
+    if not os.path.exists(path):
+        print("Creating trajectories directory {}".format(path))
+        os.mkdir(path)
+    return video_filename
--- src/train_gan.py
+++ src/train_gan.py
@@ -4,15 +4,13 @@ import os
 import sys
 
 parser = argparse.ArgumentParser()
-# The result_dir must always be specified
 parser.add_argument('--result_dir', required=True, help='Output directory for images and model checkpoints')
-# Other options have default values
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. [default: 0.5]')
 parser.add_argument('--weight_decay', type=float, default=.0, help='Optimizer L2 weight decay [default: .0]')
-parser.add_argument('--pt_loss', type=float, default=1.0, help='Multiplier for pull-away term loss [default: 1.0]')
-parser.add_argument('--fm_loss', type=float, default=1.0, help='Multiplier for feature-matching loss [default: 1.0]')
 parser.add_argument('--generator_weight', type=float, default=0.1, help='Multiplier for generator adversarial loss [default: 0.1]')
 parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train for [default: 10]')
+parser.add_argument('--cf_speed', type=float, default=1.0, help='Counterfactual optimization rate [default: 1.0]')
+parser.add_argument('--cf_max_iters', type=int, default=100, help='Counterfactual gradient descent steps [default: 100]')
 
 options = vars(parser.parse_args())
 
@@ -21,7 +19,7 @@ from dataloader import FlexibleCustomDataloader
 from training import train_gan
 from networks import build_networks, save_networks, get_optimizers
 from options import load_options, get_current_epoch
-from locking import acquire_lock, release_lock
+from counterfactual import generate_counterfactual
 
 options = load_options(options)
 dataloader = FlexibleCustomDataloader(fold='train', **options)
@@ -31,4 +29,5 @@ optimizers = get_optimizers(networks, **options)
 start_epoch = get_current_epoch(options['result_dir']) + 1
 for epoch in range(start_epoch, start_epoch + options['epochs']):
     train_gan(networks, optimizers, dataloader, epoch=epoch, **options)
+    generate_counterfactual(networks, dataloader, **options)
     save_networks(networks, epoch, options['result_dir'])
