c0a959adee5a5a4e9cc2a27552838176f9608697
Larry Neal <nealla@lwneal.com>
2018-01-30 10:13:31 -0800

    Switch to MNIST for testing, decrease job length
    10 epochs ought to be enough to see whether things are working

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 pip install -r requirements.txt
 
 # TODO: Download the correct dataset for this experiment
-python src/datasets/download_svhn.py
+#python src/datasets/download_svhn.py
+python src/datasets/download_mnist.py
 
-python src/train_gan.py --result_dir . --epochs 100
+python src/train_gan.py --result_dir . --epochs 10
