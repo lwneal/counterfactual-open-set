525457fa86cea280b9c5fe7ae590b91652049887
Larry Neal <nealla@lwneal.com>
2018-01-30 10:49:37 -0800

    Save stdout
    TODO: Display the last 10 lines for running jobs in the UI

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 #python src/datasets/download_svhn.py
 python src/datasets/download_mnist.py
 
-python src/train_gan.py --result_dir . --epochs 10
+python src/train_gan.py --result_dir . --epochs 10 | tee stdout.txt
