7bc481de009f51ecda676de24c9fbcc78d2bf411
Larry Neal <nealla@lwneal.com>
2018-01-31 10:02:49 -0800

    Modified network: denser, less convolutional
    Wider receptive field, more distributed representation, will make
    reconstruction more difficult but will allow for better samples

--- src/network_definitions.py
+++ src/network_definitions.py
@@ -41,8 +41,10 @@ class encoder32(nn.Module):
         self.bn7 = nn.BatchNorm2d(128)
         self.bn8 = nn.BatchNorm2d(128)
         self.bn9 = nn.BatchNorm2d(128)
+        self.bn9b = nn.BatchNorm2d(128)
 
-        self.conv10 = nn.Conv2d(   128,      int(latent_size / 16), 3, stride=1, padding=1)
+        self.conv9b = nn.Conv2d(   128,      128, 3, stride=2, padding=1, bias=False)
+        self.conv10 = nn.Conv2d(   128,      int(latent_size / 4), 3, stride=1, padding=1)
         #self.fc1 = nn.Linear(128*4*4, latent_size)
         self.dr1 = nn.Dropout2d(0.2)
         self.dr2 = nn.Dropout2d(0.2)
@@ -86,7 +88,12 @@ class encoder32(nn.Module):
         x = self.conv9(x)
         x = self.bn9(x)
         x = nn.LeakyReLU(0.2, inplace=True)(x)
+
+        x = self.conv9b(x)
+        x = self.bn9b(x)
+        x = nn.LeakyReLU(0.2, inplace=True)(x)
         x = self.conv10(x)
+
         x = x.view(batch_size, -1)
         #x = self.fc1(x)
         return x
@@ -96,12 +103,13 @@ class generator32(nn.Module):
     def __init__(self, latent_size=100, batch_size=64, **kwargs):
         super(self.__class__, self).__init__()
         self.latent_size = latent_size
-        #self.fc1 = nn.Linear(latent_size, 4*4*512)
-        self.conv0 = nn.ConvTranspose2d(   int(latent_size/16),      512, 3, stride=1, padding=1, bias=False)
+        self.conv0a = nn.ConvTranspose2d(int(latent_size/4),  512, 3, stride=1, padding=1, bias=False)
+        self.conv0b = nn.ConvTranspose2d(   512,      512, 4, stride=2, padding=1, bias=False)
         self.conv1 = nn.ConvTranspose2d(   512,      256, 4, stride=2, padding=1, bias=False)
         self.conv2 = nn.ConvTranspose2d(   256,      128, 4, stride=2, padding=1, bias=False)
         self.conv3 = nn.ConvTranspose2d(   128,        3, 4, stride=2, padding=1)
-        self.bn0 = nn.BatchNorm2d(512)
+        self.bn0a = nn.BatchNorm2d(512)
+        self.bn0b = nn.BatchNorm2d(512)
         self.bn1 = nn.BatchNorm2d(256)
         self.bn2 = nn.BatchNorm2d(128)
 
@@ -110,22 +118,17 @@ class generator32(nn.Module):
         self.cuda()
 
     def forward(self, x):
-        """
-        Based on Improved GAN from Salimans et al
-        For reference:
-
-        nn.batch_norm(ll.DenseLayer(gen_layers[-1], num_units=4*4*512, W=Normal(0.05), nonlinearity=nn.relu), g=None)
-        ll.ReshapeLayer(gen_layers[-1], (args.batch_size,512,4,4))
-        nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,256,8,8), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None) # 4 -> 8
-        nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,128,16,16), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None) # 8 -> 16
-        nn.weight_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,3,32,32), (5,5), W=Normal(0.05), nonlinearity=T.tanh), train_g=True, init_stdv=0.1) # 16 -> 32
-        """
         batch_size = x.shape[0]
-        x = x.resize(batch_size, int(self.latent_size / 16), 4, 4)
-        #x = self.fc1(x)
-        x = self.conv0(x)
+
+        x = x.resize(batch_size, int(self.latent_size / 4), 2, 2)
+        # 512 x 2 x 2
+        x = self.conv0a(x)
         x = nn.ReLU(inplace=True)(x)
-        x = self.bn0(x)
+        x = self.bn0a(x)
+        # 512 x 2 x 2 (still)
+        x = self.conv0b(x)
+        x = nn.ReLU(inplace=True)(x)
+        x = self.bn0b(x)
         # 512 x 4 x 4
         x = self.conv1(x)
         x = nn.ReLU(inplace=True)(x)
@@ -179,25 +182,6 @@ class multiclassDiscriminator32(nn.Module):
         self.cuda()
 
     def forward(self, x, return_features=False):
-        """
-        Based on Salimans et al
-
-        Reference:
-        ll.DropoutLayer(disc_layers[-1], p=0.2)
-        nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 64, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu))
-        nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 64, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu))
-        nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 64, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu))
-        ll.DropoutLayer(disc_layers[-1], p=0.5)
-        nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 128, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu))
-        nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 128, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu))
-        nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 128, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu))
-        ll.DropoutLayer(disc_layers[-1], p=0.5)
-        nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 128, (3,3), pad=0, W=Normal(0.05), nonlinearity=nn.lrelu))
-        nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=128, W=Normal(0.05), nonlinearity=nn.lrelu))
-        nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=128, W=Normal(0.05), nonlinearity=nn.lrelu))
-        ll.GlobalPoolLayer(disc_layers[-1])
-        nn.weight_norm(ll.DenseLayer(disc_layers[-1], num_units=10, W=Normal(0.05), nonlinearity=None), train_g=True, init_stdv=0.1)
-        """
         batch_size = len(x)
 
         x = self.dr1(x)
