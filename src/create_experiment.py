2ace1b13a24232e06b92fbb9a90def4b4e1d58f2
Larry Neal <nealla@lwneal.com>
2018-01-31 19:26:50 -0800

    Autogenerate experiment directory

--- src/create_experiment.py
+++ src/create_experiment.py
@@ -7,7 +7,7 @@ def is_true(x):
     return not not x and x not in ['false', 'False', '0']
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--result_dir', required=True, help='Output directory for images and model checkpoints')
+parser.add_argument('--result_dir', help='Output directory for images and model checkpoints')
 parser.add_argument('--dataset', required=True, help='Input filename (must be in .dataset format)')
 parser.add_argument('--hypothesis', required=True, help='A helpful description so you can remember why you trained this network')
 parser.add_argument('--latent_size', type=int, default=128, help='Size of the latent z vector [default: 128]')
@@ -17,6 +17,14 @@ parser.add_argument('--random_horizontal_flip', type=is_true, default=False, hel
 parser.add_argument('--delete_background', type=is_true, default=False, help='Delete non-foreground pixels from images [default: False]')
 
 options = vars(parser.parse_args())
+
+# TODO: more principled, with configuration
+if options.get('result_dir') is None:
+    dataset_name = options['dataset'].split('/')[-1].split('.')[0]
+    import uuid
+    random_hex = uuid.uuid4().hex[:8]
+    options['result_dir'] = '/mnt/nfs/experiments/{}_{}'.format(
+            dataset_name, random_hex)
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from dataloader import FlexibleCustomDataloader
