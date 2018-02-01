0e9bcc9451a05a894c5e1828969c94c0d6c53d29
Larry Neal <nealla@lwneal.com>
2018-02-01 10:34:54 -0800

    Add standalone generate_counterfactual script

---
+++ src/generate_counterfactual.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+import os
+import sys
+
+def is_true(x):
+    return not not x and x.lower().startswith('t')
+
+# Dataset (input) and result_dir (output) are always required
+parser = argparse.ArgumentParser()
+parser.add_argument('--result_dir', required=True, help='Output directory for images and model checkpoints')
+
+# Other options can change with every run
+parser.add_argument('--batch_size', type=int, default=64, help='Batch size [default: 64]')
+parser.add_argument('--fold', type=str, default='train', help='Fold [default: train]')
+parser.add_argument('--cf_speed', type=float, default=.01, help='Learning rate for counterfactual descent [default: .01]')
+parser.add_argument('--cf_max_iters', type=int, default=100, help='Maximum number of steps to take for CF trajectories [default: 100]')
+parser.add_argument('--start_epoch', type=int, help='Epoch to start from (defaults to most recent epoch)')
+parser.add_argument('--count', type=int, default=1, help='Number of counterfactuals to generate')
+
+options = vars(parser.parse_args())
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from dataloader import CustomDataloader
+import counterfactual
+from networks import build_networks
+from options import load_options
+
+
+options = load_options(options)
+
+
+dataloader = CustomDataloader(**options)
+
+# Batch size must be large enough to make a square grid visual
+options['batch_size'] = dataloader.num_classes + 1
+
+networks = build_networks(dataloader.num_classes, **options)
+
+for i in range(options['count']):
+    counterfactual.generate_counterfactual(networks, dataloader, **options)
