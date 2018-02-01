a5bf3c0d4d6de97a13211a3760acc31b95c4059f
Larry Neal <nealla@lwneal.com>
2018-02-01 01:42:36 -0800

    What why is --epochs 1 not working...

--- start.sh
+++ start.sh
@@ -11,8 +11,7 @@ fi
 #python src/datasets/download_celeba.py
 #python src/datasets/download_oxford102.py
 
-# Hack to eval at each epoch
+python src/train_gan.py 2>&1 | tee stdout.txt
 for i in `seq 10`; do
-    python src/train_gan.py --epochs 1 2>&1 | tee stdout.txt
-    python src/evaluate_classifier.py --result_dir . --comparison_dataset /mnt/data/svhn-59.dataset 2>&1 | tee stdout.txt
+    python src/evaluate_classifier.py --epoch $i --result_dir . --comparison_dataset /mnt/data/svhn-59.dataset 2>&1 | tee stdout.txt
 done
