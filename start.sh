38edd3415f78a085f29fb8733e1427b907c4ec06
Larry Neal <nealla@lwneal.com>
2018-02-01 17:02:14 -0800

    Lower number of epochs for some quick tests

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ fi
 #python src/datasets/download_celeba.py
 #python src/datasets/download_oxford102.py
 
-for i in `seq 30`; do
+for i in `seq 10`; do
     python src/train_gan.py --epochs 1 2>&1 | tee -a stdout.txt
     python src/evaluate_classifier.py --result_dir . --comparison_dataset /mnt/data/svhn-59.dataset 2>&1 | tee -a stdout.txt
 done
