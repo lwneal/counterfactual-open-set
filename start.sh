81ee80e3a0366a0b4ca256d6bc794e2a31e085e5
Larry Neal <nealla@lwneal.com>
2018-02-06 16:21:58 -0800

    Collapse evaluation into training

--- start.sh
+++ start.sh
@@ -19,7 +19,4 @@ fi
 #python src/datasets/download_celeba.py
 #python src/datasets/download_oxford102.py
 
-for i in `seq 100`; do
-    python src/train_gan.py --epochs 1 2>&1 | tee -a stdout.txt
-    python src/evaluate_classifier.py --result_dir . --comparison_dataset /mnt/data/svhn-59.dataset 2>&1 | tee -a stdout.txt
-done
+python src/train_gan.py --epochs 100 2>&1 | tee -a stdout.txt
