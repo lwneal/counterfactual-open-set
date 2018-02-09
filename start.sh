5431dd89d4a623850920d7b886bdf3d8f05dfc82
Larry Neal <nealla@lwneal.com>
2018-02-09 15:22:23 -0800

    Add classifier training to automatic run script

--- start.sh
+++ start.sh
@@ -20,4 +20,21 @@ if [ ! -f /mnt/data/celeba.dataset ]; then
     python src/datasets/download_celeba.py
 fi
 
-python src/train_gan.py --epochs 10 2>&1 | tee -a stdout.txt
+GAN_EPOCHS=1
+CLASSIFIER_EPOCHS=10
+
+# Train the intial generative model (E+G+D+C)
+python src/train_gan.py --epochs $GAN_EPOCHS 2>&1 | tee -a stdout.txt
+
+# Generate a number of counterfactuals
+python src/generate_counterfactual.py --result_dir .
+
+# TODO: Automatically label those counterfactuals
+# for now just copy this pre-generated set of 950 cf's
+cp /mnt/nfs/experiments/svhn-04_aux_example.dataset .
+
+# Then train a new classifier, using the aux_dataset
+python src/train_classifier.py --epochs $CLASSIFIER_EPOCHS
+
+# Then evaluate the classifier
+python src/evaluate_classifier.py --result_dir . --comparison_dataset /mnt/data/svhn-59.dataset
