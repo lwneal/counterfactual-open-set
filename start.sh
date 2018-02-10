88bea80bfb3a6631dcfe912d64a62f874a08d833
Larry Neal <nealla@lwneal.com>
2018-02-10 09:11:28 -0800

    Output all logs to stdout.txt

--- start.sh
+++ start.sh
@@ -28,14 +28,18 @@ CF_COUNT=200
 python src/train_gan.py --epochs $GAN_EPOCHS 2>&1 | tee -a stdout.txt
 
 # Generate a number of counterfactuals, in K+2 by K+2 square grids
-python src/generate_counterfactual.py --result_dir . --count $CF_COUNT
+python src/generate_counterfactual.py --result_dir . --count $CF_COUNT 2>&1 | tee -a stdout.txt
+
 
 # Automatically label the rightmost column in each grid (ignore the others)
 # TODO: Something more elegant than this?
-python src/auto_label.py --output_filename generated_images.dataset
+python src/auto_label.py --output_filename generated_images.dataset 2>&1 | tee -a stdout.txt
+
 
 # Train a new classifier, now using the aux_dataset containing the counterfactuals
-python src/train_classifier.py --epochs $CLASSIFIER_EPOCHS --aux_dataset generated_images.dataset --comparison_dataset /mnt/data/svhn-59.dataset
+python src/train_classifier.py --epochs $CLASSIFIER_EPOCHS --aux_dataset generated_images.dataset --comparison_dataset /mnt/data/svhn-59.dataset 2>&1 | tee -a stdout.txt
+
 
 # Evaluate it one more time just for good measure
-python src/evaluate_classifier.py --result_dir . --comparison_dataset /mnt/data/svhn-59.dataset
+python src/evaluate_classifier.py --result_dir . --comparison_dataset /mnt/data/svhn-59.dataset 2>&1 | tee -a stdout.txt
+
