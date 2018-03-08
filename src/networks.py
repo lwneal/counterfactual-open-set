e9e37d44fb44305371638ec49d5ff0c068321717
Larry Neal <nealla@lwneal.com>
2018-03-07 17:21:53 -0800

    Properly load networks when no epoch specified

--- src/networks.py
+++ src/networks.py
@@ -67,7 +67,7 @@ def get_pth_by_epoch(result_dir, name, epoch=None):
     suffix = '.pth'
     if epoch is not None:
         suffix = 'epoch_{:04d}.pth'.format(epoch)
-    files = [f for f in files if f == '{}_{}'.format(name, suffix)]
+    files = [f for f in files if '{}_epoch'.format(name) in f]
     if not files:
         return None
     files = [os.path.join(checkpoint_path, fn) for fn in files]
