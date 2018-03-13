19c114c432e2d84ce9f2b369d8177fb387986689
Larry Neal <nealla@lwneal.com>
2018-03-12 18:16:18 -0700

    Fix GENERATOR_MODE

--- start.sh
+++ start.sh
@@ -49,15 +49,15 @@ python src/evaluate_classifier.py --result_dir . --mode weibull
 # For 100, 200, ... generated examples:
 for i in `seq 10`; do
     # Generate a number of counterfactuals, in K+2 by K+2 square grids
-    python src/generate_${MODE}.py --result_dir . --count $CF_COUNT
+    python src/generate_${GENERATOR_MODE}.py --result_dir . --count $CF_COUNT
 
     # Automatically label the rightmost column in each grid (ignore the others)
-    python src/auto_label_${MODE}.py --output_filename generated_images_${MODE}.dataset
+    python src/auto_label_${GENERATOR_MODE}.py --output_filename generated_images_${GENERATOR_MODE}.dataset
 
     # Train a new classifier, now using the aux_dataset containing the counterfactuals
-    python src/train_classifier.py --epochs $CLASSIFIER_EPOCHS --aux_dataset generated_images_${MODE}.dataset
+    python src/train_classifier.py --epochs $CLASSIFIER_EPOCHS --aux_dataset generated_images_${GENERATOR_MODE}.dataset
 
     # Evaluate it one more time just for good measure
-    python src/evaluate_classifier.py --result_dir . --aux_dataset generated_images_${MODE}.dataset
+    python src/evaluate_classifier.py --result_dir . --aux_dataset generated_images_${GENERATOR_MODE}.dataset
 done
 python src/evaluate_classifier.py --result_dir . --mode weibull-kplusone
