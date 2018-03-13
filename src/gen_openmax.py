d77bfdf966d5de6fe30a33353d885f6e5c667288
Larry Neal <nealla@lwneal.com>
2018-03-13 05:49:26 -0700

    Add rejection sampling to Generative OpenMax
    This is not quite the same as Ge et al. because the generator is not
    conditional on class labels. So, instead of using a class label vs
    the output classification as a metric of how 'open' a generated
    interpolation is, I use confidence thresholding.

--- src/gen_openmax.py
+++ src/gen_openmax.py
@@ -32,26 +32,37 @@ def generate(networks, dataloader, **options):
     netC = networks['classifier_k']
     netE = networks['encoder']
 
-    # Start with randomly-selected images from the dataloader
-    start_images, _ = dataloader.get_batch()
-    end_images, _ = dataloader.get_batch()
-
-    # Interpolate between them in latent space
-    z_0 = netE(start_images, gan_scale)
-    z_1 = netE(end_images, gan_scale)
-    theta = np.random.uniform(size=len(z_0))
-    theta = Variable(torch.FloatTensor(theta)).cuda()
-    theta = theta.unsqueeze(-1)
-    z_interp = theta * z_0 + (1 - theta) * z_1
-
-    images = netG(z_interp, gan_scale)
-    images = images.data.cpu().numpy()
-
     # Ge et al: Selection of Generated Samples
-    # A conditional GAN is used to generate images
-    # Incorrectly predicted samples are selected as candidates
+    # A conditional GAN is used to generate images, and
+    # incorrectly predicted samples are selected as candidates
+    # Here, we don't use a conditional GAN, so instead of
+    # incorrect prediction we take the predicted samples
+    # with classification confidence less than a threshold
+    classification_threshold = .5
+    openset_images = []
+    while len(openset_images) < 64:
+        # Start with randomly-selected images from the dataloader
+        start_images, _ = dataloader.get_batch()
+        end_images, _ = dataloader.get_batch()
 
-    images = np.array(images).transpose((0,2,3,1))
+        # Interpolate between them in latent space
+        z_0 = netE(start_images, gan_scale)
+        z_1 = netE(end_images, gan_scale)
+        theta = np.random.uniform(size=len(z_0))
+        theta = Variable(torch.FloatTensor(theta)).cuda()
+        theta = theta.unsqueeze(-1)
+        z_interp = theta * z_0 + (1 - theta) * z_1
+
+        images = netG(z_interp, gan_scale)
+        preds = F.softmax(netC(images))
+        confidence = preds.max(dim=1)[0]
+        images = images.data.cpu().numpy()
+        for idx, conf in enumerate(confidence.data.cpu().numpy()):
+            if conf < classification_threshold:
+                openset_images.append(images[idx])
+    openset_images = openset_images[:64]
+
+    images = np.array(openset_images).transpose((0,2,3,1))
     dummy_class = 0
     video_filename = make_video_filename(result_dir, dataloader, dummy_class, dummy_class, label_type='grid')
 
